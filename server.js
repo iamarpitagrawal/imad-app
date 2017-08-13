3fd52dd08c88be41d389d8ece45a37bb9b27ed94
iamarpitagrawal <30876764+iamarpitagrawal@users.noreply.github.com>
2017-08-13 23:41:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,59 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articalOne = { 
+   title:'Artical one| Arpit Agrawal',
+    heading:'Artical one',
+    date: 'sep 5,2016',
+    content:`<p>
+                this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.
+            </p>
+            <p>
+                this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.
+            </p>
+            <p>
+                this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.this is the content for my first artical.
+            </p>`
+    };
+function createTemplate(data)
+{var titledata.title
+var date = data.date;
+var heading = data.heading;
+var content = data.content;
+var htmlTemplate =
+    <html>
+    <head>
+            <title>
+              ${title}
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <link href="/ui/style.css" rel="stylesheet" />
+            
+   </head>
+    
+    <body>
+        <div class="container">
+          <div>
+                <a href="/">Home</a>
+            
+             </div>
+            <hr/>
+            <h3>
+            ${heading}
+            </h3>
+            <div>
+            ${date}
+            </div>
+            <div>
+            ${content}
+            </div>
+        </div>
+    </body>
+    
+    
+</html>
 
+};
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
